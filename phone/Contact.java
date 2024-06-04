2195b7124e5f50b04c33c6a0f7e26d5270febb30
Illay Kaye <illaykaye@gmail.com>
2024-06-04 10:22:37 +0300

    First commit

---
+++ phone/Contact.java
@@ -0,0 +1,35 @@
+package phone;
+
+public class Contact {
+	private String name;
+	private String phoneNumber;
+	
+	public Contact(String name, String phoneNumber) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (!obj.getClass().equals(Contact.class))
+			return false;
+		Contact other = (Contact)obj;
+		return other.name.equals(this.name) && other.phoneNumber.equals(this.phoneNumber);
+	}
+}
---
+++ phone/Phonebook.java
@@ -0,0 +1,56 @@
+package phone;
+
+import java.util.ArrayList;
+
+public class Phonebook {
+	private ArrayList<Contact> contacts;
+	
+	public Phonebook() {
+		this.contacts = new ArrayList<Contact>();
+	}
+	
+	public void run() {
+		
+	}
+	
+	private void addContact(Contact contact) {
+		this.contacts.add(contact);
+	}
+	
+	private void removeContact(String name) {
+		for (int i = 0; i < this.contacts.size(); ++i)
+			if (this.contacts.get(i).getName().equals(name)) {
+				this.contacts.remove(i);
+				return;
+			}
+	}
+	
+	private void printAllContacts() {
+		
+	}
+	
+	private void nameLexicographicSort() {
+		this.contacts.sort((a, b) -> a.getName().compareTo(b.getName()));
+	}
+	
+	private void numberNumericSort() {
+		this.contacts.sort((a, b) -> b.getPhoneNumber().compareTo(a.getPhoneNumber()));
+	}
+	
+	private void removeDuplicateContacts() {
+		
+	}
+	
+	private void reverseOrder() {
+		
+	}
+	
+	// NOTE: This should be combined with "printAllContacts"
+	private void saveToFile() {
+		
+	}
+	
+	private void loadFromFile() {
+		
+	}
+}
